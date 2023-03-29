287b7ec1102c2817ba240aa9d762ef77c6ab5349
eruotolo <edgardoruotolo@outlook.com>
2023-03-29 18:48:08 -0300

    Entrega de Ejercicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ListaRuotolo';
+
+  alumnos:string[] = [
+    'Álvarez Cuartas, María',
+    'Álvarez García, Benito',
+    'Álvarez Rodríguez, Emilio Marcelino',
+    'Álvarez Tamargo, María',
+    'Astray Sánchez, Julio',
+    'Barrera Cuervo, Moisés',
+    'Blanco Cué, Graciela',
+    'Campo Rilo, Paula Del',
+    'Cortés-echánove García, Jaime María',
+    'Díaz Ubieta, Íñigo',
+    'Fernández Espina, Bruno',
+    'Fernández Fernández-nespral, Héctor',
+    'Fernández Somoano, Pedro',
+    'García Fernández, Alejandra',
+    'García García, Beatriz',
+    'García García, Pablo',
+    'García González, Guillermo',
+    'García Suárez, Adrián',
+    'González García, Antonio',
+    'González Tomás, Maria Adelaida',
+    'Iglesias Santirso, Lucía',
+    'Martínez Álvarez, Juan Manuel',
+    'Martínez Morán, Paula',
+    'Mas Andrés, Virginia Maria',
+    'Menéndez Gutiérrez, Guillermo',
+    'Nicieza Fernández, Víctor',
+    'Ornosa Miravalles, Peru',
+    'Ortega Moro, Oriol',
+    'Pérez Fernández, Fabián',
+    'Pérez Gutiérrez, Fernando',
+    'Quevedo Alvaré, María',
+    'Rodríguez Alonso, David',
+    'Rodríguez Peláez, Borja',
+    'Rodríguez Rodríguez, Javier',
+    'Rosal Fraga, Joaquín',
+    'Ruiz Gómez, Miriam',
+    'Sanz De Pedro, Héctor Miguel',
+    'Serrano Fernández De Travanco, Álvaro',
+    'Torrecillas Reig, Ramón',
+    'Villa Carro, Mario',
+    'Zapico Álvarez, Jorge'
+  ]
+
 }
